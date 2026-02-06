656430d8bd597b85fdd329052a7fb9a578a0c2f2
MarkusJakobR <mcrecaplaza@up.edu.ph>
2026-02-06 12:50:42 +0800

    feat: added oil.nvim for easier editing and manipulating of the directory IF NEEDED

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,14 +1,23 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
-		"nvim-tree/nvim-web-devicons", -- optional, but recommended
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false, -- neo-tree will lazily load itself
+		cmd = "Neotree",
+		keys = {
+			{ "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle file explorer" },
+		},
 	},
-	lazy = false, -- neo-tree will lazily load itself
-	cmd = "Neotree",
-	keys = {
-		{ "<leader>e", "<cmd>Neotree toggle<CR>", desc = "Toggle file explorer" },
+	{
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup()
+			vim.keymap.set("n", "<leader>E", "<cmd>Oil --float<CR>", { desc = "O[il]pen parent directory" })
+		end,
 	},
 }
